8a6c459f4f8a789e3d8a97407e080a014896eb31
JT-B <jtbellavia03@gmail.com>
2021-04-22 20:15:02 -0500

    Registration set up

---
+++ src/main/java/org/LaunchCode/JTBLiftoff/Model/User.java
@@ -0,0 +1,58 @@
+package org.LaunchCode.JTBLiftoff.Model;
+
+import com.sun.istack.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class User{
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User that = (User) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+}
---
+++ src/main/java/org/LaunchCode/JTBLiftoff/Model/dto/RegisterFormDTO.java
@@ -0,0 +1,14 @@
+package org.LaunchCode.JTBLiftoff.Model.dto;
+
+public class RegisterFormDTO extends LoginFormDTO{
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
